e0500abbdfc45dc2bc61d3b290465250e95012d8
Aviv <avisrael11@gmail.com>
2017-12-06 18:32:57 +0200

    Let's begin!

---
+++ src/test/java/bgu/spl/a2/PromiseTest.java
@@ -0,0 +1,52 @@
+package bgu.spl.a2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PromiseTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void isResolved() throws Exception {
+    }
+
+    @Test
+    public void testResolve() throws Exception {
+        try{
+            Promise<Integer> p = new Promise<>();
+            p. resolve (5);
+            try{
+                p. resolve (6);
+                assert.fail ();
+            catch(IllegalStateException ex){ int x = p.get();
+                    assertEquals(x,5);
+                }
+            catch(Exception ex){
+                    assert.fail();
+
+                }
+        }
+        catch(Exception ex){
+                Assert.fail();
+        }
+    }
+
+    @Test
+    public void subscribe() throws Exception {
+    }
+
+}
---
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -0,0 +1,30 @@
+package bgu.spl.a2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VersionMonitorTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getVersion() throws Exception {
+    }
+
+    @Test
+    public void inc() throws Exception {
+    }
+
+    @Test
+    public void await() throws Exception {
+    }
+
+}
