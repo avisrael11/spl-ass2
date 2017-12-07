affaaf14be44a1f95fbf12ed7e8571518ec1a227
Aviv <avisrael11@gmail.com>
2017-12-07 18:31:15 +0200

    pronise shit

--- src/test/java/bgu/spl/a2/PromiseTest.java
+++ src/test/java/bgu/spl/a2/PromiseTest.java
@@ -4,21 +4,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-//import junit.framework.Assert;
-//import junit.framework.TestCase;
 import org.junit.Assert;
 
 import static org.junit.Assert.*;
-/*
-*
-*
- */
+
 public class PromiseTest {
 
 
     private Promise<Integer> p;
-
-
 
     @Before
     public void setUp() throws Exception {
