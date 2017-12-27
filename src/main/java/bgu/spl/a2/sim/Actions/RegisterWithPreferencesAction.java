fb5a522298b3354642d947dfa4d6dd03abfe0a06
oradrok <oradrok@users.noreply.github.com>
2017-12-27 16:59:33 +0200

    register with preferences

--- src/main/java/bgu/spl/a2/sim/Actions/RegisterWithPreferencesAction.java
+++ src/main/java/bgu/spl/a2/sim/Actions/RegisterWithPreferencesAction.java
@@ -1,9 +1,56 @@
 package bgu.spl.a2.sim.Actions;
 
 import bgu.spl.a2.Action;
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
 
-public class RegisterWithPreferencesAction extends Action {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterWithPreferencesAction extends Action<Boolean> {
+
+    private String studentId;
+    private List<String> courses;
+    private List<Integer> grades;
+
+    private final String actionName = "Register With Preferences";
+
+    public RegisterWithPreferencesAction(String studentId, List<String> courses, List<Integer> grades ){
+        this.studentId  = studentId;
+        this.courses    = courses;
+        this.grades     = grades;
+
+        setActionName(actionName);
+        Id = studentId;
+    }
+
+
     public void start(){
+        privateState.addRecord(actionName);
 
+        tryRegisterSequently();
+    }
+
+    private void tryRegisterSequently(){
+
+        if (courses.size() > 0) {
+            String courseName = courses.remove(0);
+            Action<Boolean> participateInCourse = new ParticipateInCourseAction(studentId, courseName, grades.remove(0));
+
+            sendMessage(participateInCourse, courseName, new CoursePrivateState());
+            List<Action<Boolean>> actions = new ArrayList<>();
+            actions.add(participateInCourse);
+
+            then(actions, ()->{
+                if(actions.get(0).getResult().get()){
+                    complete(true);
+                }
+                else {
+                    tryRegisterSequently();
+                }
+            });
+        }
+        else {
+            complete(false);
+        }
     }
 }
